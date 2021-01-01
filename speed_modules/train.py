387d7621ac84282595480f5d4074cc84dda1a1b2
nturumel <nturumel@alumni.cmu.edu>
2021-01-01 17:47:50 -0600

    fix: changed model

--- speed_modules/train.py
+++ speed_modules/train.py
@@ -17,9 +17,10 @@ import os
 def get_model():
     model = Sequential()
     model.add(Input(shape = (INPUT_SHAPE)))
-    model.add(Dense(5, activation='relu'))
-    model.add(Dense(3, activation='relu'))
     model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
     model.add(Dense(1, activation='relu'))
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy', 'MeanSquaredError'])
     return model
@@ -36,13 +37,19 @@ def train(train_input = SAVE_ARRAY_FILE, train_output = TRAIN_OUTPUT):
     )
     # split 
     X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=1)
-    scaler= StandardScaler()
 
+    scaler= StandardScaler()
     print((X_train.reshape(-1, X_train.shape[-1])).shape)
     X_train = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(X_train.shape)
     X_val = scaler.transform(X_val.reshape(-1, X_val.shape[-1])).reshape(X_val.shape)
-
     print(X_train.shape)
+
+    scaler_Y = StandardScaler()
+    print(Y_train.shape)
+    Y_train = scaler_Y.fit_transform(Y_train)
+    Y_val = scaler_Y.transform(Y_val)
+    print("Final scaler Y: ", scaler_Y)
+
     '''
      for i in range(x):
         for j in range(y):
@@ -50,7 +57,7 @@ def train(train_input = SAVE_ARRAY_FILE, train_output = TRAIN_OUTPUT):
                 if a[i, j, k] > 1 or a[i, j, k] < 0:
                     print("problem, value = %f, indice = %d, %d, %d" % (a[i, j, k], i, j, k))
     '''
-    
+
     # callbacks
     checkpoint = ModelCheckpoint(MODEL_PATH, verbose=1, monitor='val_accuracy', save_best=True)
     logdir = os.path.join(LOG_DIR, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
